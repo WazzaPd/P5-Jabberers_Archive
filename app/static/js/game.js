fcdead10039f51d92398ad688cb6278a8989f4f6
Jeremy-Kwok <Jeremykwok917@gmail.com>
2023-06-06 09:49:35 -0400

    6/6

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,61 +1,93 @@
+let gameScene = new Phaser.Scene('Game')
+
 // Game configuration
 var config = {
+    type: Phaser.AUTO,
     width: 800,
     height: 600,
-    type: Phaser.AUTO,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 }, // Set the gravity value as needed
-            debug: false // Set to true if you want to show physics bodies
-        }
-    },
-    scene: {
+    parent: 'phaser-game',
+    scene: gameScene
+    
+    /*
+    {
         preload: preload,
         create: create,
         update: update
     }
+    */
+    ,
+    physics: {
+        default: 'arcade',
+        arcade: { // Set the gravity value as needed
+            debug: true // Set to true if you want to show physics bodies
+        }
+    }
 };
 
 // Create the Phaser game instance
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
-// Player variables
-var player;
+gameScene.preload = function() {
+    this.load.image("kirby", "assets/kirby.png")
+    this.load.image("ground", "assets/ground.png")
+}
+
+gameScene.create = function() {
+    this.add.sprite(100, 100, "kirby");
+    this.add.sprite()
+}
+
+/*
+// kirby variables
+var kirby;
 var cursors;
 
-// Preload game assets
+// Preload game assets and sounds
 function preload() {
+    this.load.image("kirby", "kirby.jpeg")
+    this.load.image("ground", "ground.jpeg")
     // No assets to preload for this example
 }
 
 // Create game objects and initialize the game
 function create() {
-    // Create the player sprite as a rectangle shape
-    player = this.add.rectangle(400, 300, 50, 50, 0xff0000);
-    this.physics.world.enable(player);
-    player.body.setCollideWorldBounds(true); // Prevent player from going off-screen
+    // Create kirby sprite
+    this.add.sprite(100, 100, "kirby");
+    let kirby = this.physics.add.sprite(this.sys.game.config.width/2, this.sys.game.config.height/2, "kirby")
+    kirby.setGravityY(100);
 
-    // Enable cursor keys for player movement
+    let groundX = this.sys.game.config.width/2;
+    let groundY = this.sys.game.config.height * .95;
+    let ground = this.physics.add.sprite(groundX, groundY, "ground");
+    ground.displayWidth = this.sys.game.config.width;
+
+    this.physics.add.collider(kirby, ground)
+    ground.setImmovable();
+    //this.physics.world.enable(kirby);
+    //kirby.body.setCollideWorldBounds(true); // Prevent kirby from going off-screen
+
+    // Enable cursor keys for kirby movement
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 // Update the game state
 function update() {
-    // Player movement
+    // kirby movement
     if (cursors.left.isDown) {
-        player.body.velocity.x = -200;
+        kirby.body.velocity.x = -200;
     } else if (cursors.right.isDown) {
-        player.body.velocity.x = 200;
+        kirby.body.velocity.x = 200;
     } else {
-        player.body.velocity.x = 0;
+        kirby.body.velocity.x = 0;
     }
 
     if (cursors.up.isDown) {
-        player.body.velocity.y = -200;
+        kirby.body.velocity.y = -200;
     } else if (cursors.down.isDown) {
-        player.body.velocity.y = 200;
+        kirby.body.velocity.y = 200;
     } else {
-        player.body.velocity.y = 0;
+        kirby.body.velocity.y = 0;
     }
 }
+
+*/
