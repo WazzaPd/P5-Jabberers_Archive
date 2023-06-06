262c0551b2a474e1881ea9088499d88a16de58b2
Aahan Mehta <aahanmehta@Aahans-MacBook-Pro.local>
2023-06-06 13:43:13 -0400

    button

--- app/__init__.py
+++ app/__init__.py
@@ -31,11 +31,11 @@ c.execute(
 db.commit()  # save changes
 #-------------------------DataBase-------------------------
 
-@app.route('/')
+@app.route('/',)
 def index():
     return render_template('index.html')
 
-@app.route('/game')
+@app.route('/game',  methods = ["POST"])
 def game():
     return render_template('game.html')
 
