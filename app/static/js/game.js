0a93e3caecdf212d78d1650099041054404350f1
Jeremy-Kwok <Jeremykwok917@gmail.com>
2023-06-06 12:08:19 -0400

    6/6 New bug: Images can't render

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,3 +1,92 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: "canvas",
+    physics: {
+        default: "arcade",
+        arcade: { 
+            gravity: { y: 1000 }, // Set the gravity value as needed
+            debug: true // Set to true if you want to show physics bodies
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+var kirby;
+var ground;
+
+function preload() {
+    //Preload sprites
+    this.load.image("kirby", "assets/kirby.png")
+    this.load.image("ground", "assets/ground.png")
+}
+
+function create() {
+    //Create kirby
+    kirby = this.add.sprite(100, 100, "kirby");
+
+    //Make bouncy kirby
+    kirby.setBounce(0.2);
+
+    //Laws of Physics
+    kirby.setCollideWorldBounds(true);
+
+    //Create floating islands
+    islands = this.physics.add.staticGroup();
+    islands.create(0, 200, "ground");
+    islands.create(400, 400, "ground");
+
+    //Let kirby crash into the wall
+    this.physics.add.collider(kirby, islands);
+
+    //Make camera focus kirby
+    this.cameras.main.startFollow(kirby);
+
+    goal = this.physics.add.sprite(400, 0, "goal")
+
+    //Enable cursor keys for kirby movement
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+
+    //Kirby Freeze
+    kirby.setVelocityX(0);
+
+    //Kirby MOVE
+    if (cursors.left.isDown){
+        kirby.setVelocityX(-150);
+    } else if (cursors.right.isDown){
+        kirby.setVelocityX(150);
+    }
+
+    if (cursors.up.isDown){
+        if (kirby.body.touching.down){
+            kirby.setVelocityY(-400);
+        }
+    } 
+
+    //Adjust camera according to kirby's height
+    this.cameras.main.scrollY = kirby.y - this.cameras.main.height/2;
+
+    //Check if the Kirby reaches the goal
+    if (Phaser.Geom.Intersects.RectangleToRectangle(kirby.getBounds(), goal.getBounds())){
+        winGame();
+    }
+}
+
+function winGame() {
+    console.log("YOU FINALLY WON!");
+}
+
+/*
 let gameScene = new Phaser.Scene('Game')
 
 // Game configuration
@@ -5,17 +94,11 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    parent: 'phaser-game',
-    scene: gameScene
-    
-    /*
     {
         preload: preload,
         create: create,
         update: update
-    }
-    */
-    ,
+    },
     physics: {
         default: 'arcade',
         arcade: { // Set the gravity value as needed
@@ -53,13 +136,13 @@ function preload() {
 function create() {
     // Create kirby sprite
     this.add.sprite(100, 100, "kirby");
-    let kirby = this.physics.add.sprite(this.sys.game.config.width/2, this.sys.game.config.height/2, "kirby")
+    let kirby = this.physics.add.sprite(this.sys.this.config.width/2, this.sys.this.config.height/2, "kirby")
     kirby.setGravityY(100);
 
-    let groundX = this.sys.game.config.width/2;
-    let groundY = this.sys.game.config.height * .95;
+    let groundX = this.sys.this.config.width/2;
+    let groundY = this.sys.this.config.height * .95;
     let ground = this.physics.add.sprite(groundX, groundY, "ground");
-    ground.displayWidth = this.sys.game.config.width;
+    ground.displayWidth = this.sys.this.config.width;
 
     this.physics.add.collider(kirby, ground)
     ground.setImmovable();
