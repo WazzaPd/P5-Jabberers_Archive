b1d2b8968ce09bfa3da8bb40f14195e47abbbc5a
Jeremy-Kwok <Jeremykwok917@gmail.com>
2023-06-07 10:59:35 -0400

    6/7 game is oddly not running and stuck on loading

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,8 +1,20 @@
+const viewWidth = 500;
+const viewHeight = 500;
+const mapWidth = 1500;
+const mapHeight = 3000;
+const scaleRatioX = 0.1 //viewWidth / mapWidth;
+const scaleRatioY = 0.1 //viewHeight / mapHeight;
+let islandsGroup;
+
 var config = {
     type: Phaser.AUTO,
-    width: 1500,
-    height: 2000,
-    parent: "canvas",
+    
+    scale: {
+        //mode: Phaser.Scale.FIT,
+        parent: "canvas",
+        width: viewWidth,
+        height: viewHeight,
+    },
     physics: {
         default: 'arcade',
         arcade: { 
@@ -19,46 +31,68 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-//var kirby;
-//var ground;
-
 function preload() {
     //Preload sprites
-    this.load.image("kirby", "static/assets/kirby.png");
-    this.load.image("ground", "static/assets/ground.png");
-    this.load.image("goal", "static/assets/goal.png");
+    this.load.image("kirby", "static/assets/kirby.png");   // 689x685
+    this.load.image("ground", "static/assets/ground.png"); // 333x32
+    this.load.image("goal", "static/assets/goal.png");     // 512x512
+    this.load.image("lava", "static/assets/lava.png");     // 487x103
 }
 
 function create() {
+
+    //Create lava floor
+    lava = this.add.sprite(mapWidth/2, mapHeight, "lava"); //this.sys.game.config.height-200, "lava");
+    lava.setScale(10, 1);
+
+    //Create islandsGroup
+    islandsGroup = this.physics.add.staticGroup();
+
+    
+    //Create floating islands
+    const islands = generateIslands();
+    islands.forEach((island) => {
+        createIsland(island.x, island.y, island.width, 32);
+        //const islandSprite = this.add.sprite(island.x, island.y, "ground");
+        //islandSprite.setScale(island.width/islandSprite.width, island.height/islandSprite.height);
+    });
+
+    /*
+    islands = this.physics.add.staticGroup();
+    islands.create(700, 2800, "ground");
+    islands.create(1300, 2600, "ground");
+    islands.create(800, 2200, "ground");
+    */
+
+    //create goal
+    goal = this.physics.add.sprite(900,1800, "goal");
+    goal.setScale(0.2,0.2);
+    this.physics.add.collider(goal, islandsGroup);
+
     //Create kirby
-    kirby = this.physics.add.sprite(600, 1900, "kirby");
+    kirby = this.physics.add.sprite(mapWidth/2, 2700, "kirby");
     kirby.setScale(0.2,0.2);
 
     //Make bouncy kirby
     kirby.setBounce(0.2);
 
     //Laws of Physics
-    kirby.setCollideWorldBounds(true);
+    //kirby.setCollideWorldBounds(true);
 
     //Enable cursor keys for kirby movement
     cursors = this.input.keyboard.createCursorKeys();
 
-    //Create floating islands
-    islands = this.physics.add.staticGroup();
-    islands.create(700, 2000, "ground");
-    islands.create(1300, 1800, "ground");
-    islands.create(800, 1400, "ground");
-
     //Let kirby crash into the wall
-    this.physics.add.collider(kirby, islands);
+    this.physics.add.collider(kirby, islandsGroup);
+
+    //this.cameras.main.setOrigin(this.sys.game.config.width/2, 2700)
+    this.cameras.main.setBounds(0, 0, mapWidth, mapHeight);
+
+    this.cameras.main.setViewport(0, 0, 500, 500);
+    this.cameras.main.setZoom(Math.min(scaleRatioX, scaleRatioY));
 
     //Make camera focus kirby
     this.cameras.main.startFollow(kirby);
-
-    //create goal
-    goal = this.physics.add.sprite(900,1000, "goal");
-    goal.setScale(0.2,0.2);
-    this.physics.add.collider(goal, islands);
 
 }
 
@@ -87,8 +121,68 @@ function update() {
     if (Phaser.Geom.Intersects.RectangleToRectangle(kirby.getBounds(), goal.getBounds())){
         winGame();
     }
+
+    if (Phaser.Geom.Intersects.RectangleToRectangle(kirby.getBounds(), lava.getBounds())){
+        loseGame();
+    }
+}
+
+function generateIslands() {
+    const islands = [];
+
+    var islandX;
+    var islandY = mapHeight - 32;
+    let lastIslandX = 0;
+    let lastIslandY = mapHeight - 32;
+    const maxGap = 500;
+    const minGap = 200;
+    var currentHeight = mapHeight;
+    var islandWidth
+    var lastIslandWidth = 150;
+
+    while (lastIslandY > 0){
+        //333x32
+
+        islandWidth = Math.floor(Math.random() * 200) + 50; //Random width btwn 50 and 250
+        var gap = Math.floor(Math.random() * (maxGap - minGap + 1)) + minGap;
+        var fiftyChance = (Math.random() < 0.5 ? -1 : 1);
+
+        //keep islandX within map bounds
+        islandX = lastIslandX + (fiftyChance * (lastIslandWidth + gap));
+        while (islandX < 0 || islandX > mapWidth){
+            islandX = lastIslandX + (fiftyChance * (lastIslandWidth + gap));
+        }
+
+        islandY = lastIslandY - Math.floor(Math.random() * 200 + 100);
+
+        //push values to constructor
+        islands.push({
+            x: islandX,
+            y: islandY,
+            width: islandWidth,
+            height: 32
+        });
+
+        //push the creation data of last island
+        lastIslandX = islandX;
+        lastIslandY = islandY;
+        lastIslandWidth = islandWidth;
+    }
+    
+    return islands;
+}
+
+function createIsland(x, y, width, height) {
+    console.log("x: " + x + " y: " + y + " width: " + width + " height " + height);
+    const island = islandsGroup.create(x, y, "ground");
+    island.setSize(width, height);
 }
 
 function winGame() {
     console.log("YOU FINALLY WON!");
 }
+
+function loseGame() {
+    console.log("You died. You suck!");
+}
+
